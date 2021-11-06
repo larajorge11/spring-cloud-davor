b69461c4d08ff2ee36e695ae5b1fedfa32f06936
Jorge Lara <larajorge11@gmail.com>
2021-11-06 07:54:39 -0500

    feat(Initial commit, initialize project)

---
+++ src/main/java/com/davor/springcloud/DavorSpring/DavorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.davor.springcloud.DavorSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavorSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davor/springcloud/DavorSpring/DavorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davor.springcloud.DavorSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
